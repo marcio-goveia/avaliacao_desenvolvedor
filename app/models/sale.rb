b10a5a50db870f4a74c55a882c59aa12a06dbe67
Marcio Goveia <marciosg4@gmail.com>
2021-07-13 19:54:29 -0300

    refactor: model validations

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,4 +1,8 @@
 class Sale < ApplicationRecord
+  validates_presence_of :buyer, :description, :address, :provider
+  validates :unit_price, presence: true, numericality: { greater_than: 0}
+  validates :quantity, presence: true, numericality: { greater_than: 0, only_integer: true}
+
   def self.import(file)
     CSV.foreach(file.path, headers: true, col_sep: "\t") do |row|  
       Sale.create! row.to_hash
@@ -8,6 +12,5 @@ class Sale < ApplicationRecord
   def self.total_sale
     Sale.all.reduce(0) { |total, sale| total + (sale.quantity * sale.unit_price)}
   end
-
 
 end

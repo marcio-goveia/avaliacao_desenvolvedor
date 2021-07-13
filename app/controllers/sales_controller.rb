6ee15474c7592d9e246878db82db6384cc69a76a
Marcio Goveia <marciosg4@gmail.com>
2021-07-13 02:18:32 -0300

    feat: add import file in sale's controller

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,10 +1,22 @@
 class SalesController < ApplicationController
-
+  before_action :check_file_presence, only: [:import]
   # GET /sales or /sales.json
   def index
     @sales = Sale.all
+    @total_sale = Sale.total_sale
   end
 
   def import
+    csv_file = params[:file]
+    Sale.import(csv_file)
+
+    redirect_to root_url, notice: "Sales successfully imported!"
+  end
+
+  private
+  def check_file_presence
+    if params[:file].nil?
+      redirect_to root_url, notice: "A file must be selected!"
+    end
   end
 end
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,13 @@
 class Sale < ApplicationRecord
+  def self.import(file)
+    CSV.foreach(file.path, headers: true, col_sep: "\t") do |row|  
+      Sale.create! row.to_hash
+    end
+  end
+
+  def self.total_sale
+    Sale.all.reduce(0) { |total, sale| total + (sale.quantity * sale.unit_price)}
+  end
+
+
 end

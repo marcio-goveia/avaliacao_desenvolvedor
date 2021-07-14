c6f417dcae33f740d847b490bd1f6e3df458a02c
Marcio Goveia <marciosg4@gmail.com>
2021-07-14 00:03:27 -0300

    tests: test implementations

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -3,11 +3,14 @@ class Sale < ApplicationRecord
   validates :unit_price, presence: true, numericality: { greater_than: 0}
   validates :quantity, presence: true, numericality: { greater_than: 0, only_integer: true}
 
-  def self.import(file)
+  def old_import(file)
     CSV.foreach(file.path, headers: true, col_sep: "\t") do |row|
       cols = Sale.new.attributes.except("id", "created_at", "updated_at").keys
       sale = Sale.new
-      cols.each_with_index { |item, index| sale[item] = row[index] }
+    
+      cols.each_with_index do |item, index|
+        sale[item] = row[index]
+      end
       sale.save
     end
   end
--- spec/requests/sales_spec.rb
+++ spec/requests/sales_spec.rb
@@ -13,117 +13,27 @@
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
 RSpec.describe "/sales", type: :request do
-  # Sale. As you add validations to Sale, be sure to
-  # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
-
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
 
   describe "GET /index" do
     it "renders a successful response" do
-      Sale.create! valid_attributes
       get sales_url
       expect(response).to be_successful
     end
   end
 
-  describe "GET /show" do
-    it "renders a successful response" do
-      sale = Sale.create! valid_attributes
-      get sale_url(sale)
-      expect(response).to be_successful
+  describe "POST /import" do
+    it "import a valid txt file" 
+      # valid_file = fixture_file_upload('files/valid.txt', 'text/plain')
+      # post import_sales_path file: valid_file
+      # expect(response).to be_successful
+      #
+
+    it "not import a empty txt file" do
+      post import_sales_path
+      expect(response).to_not be_successful
     end
+
+    it "not import invalid lines of text file"
   end
 
-  describe "GET /new" do
-    it "renders a successful response" do
-      get new_sale_url
-      expect(response).to be_successful
-    end
-  end
-
-  describe "GET /edit" do
-    it "render a successful response" do
-      sale = Sale.create! valid_attributes
-      get edit_sale_url(sale)
-      expect(response).to be_successful
-    end
-  end
-
-  describe "POST /create" do
-    context "with valid parameters" do
-      it "creates a new Sale" do
-        expect {
-          post sales_url, params: { sale: valid_attributes }
-        }.to change(Sale, :count).by(1)
-      end
-
-      it "redirects to the created sale" do
-        post sales_url, params: { sale: valid_attributes }
-        expect(response).to redirect_to(sale_url(Sale.last))
-      end
-    end
-
-    context "with invalid parameters" do
-      it "does not create a new Sale" do
-        expect {
-          post sales_url, params: { sale: invalid_attributes }
-        }.to change(Sale, :count).by(0)
-      end
-
-      it "renders a successful response (i.e. to display the 'new' template)" do
-        post sales_url, params: { sale: invalid_attributes }
-        expect(response).to be_successful
-      end
-    end
-  end
-
-  describe "PATCH /update" do
-    context "with valid parameters" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
-
-      it "updates the requested sale" do
-        sale = Sale.create! valid_attributes
-        patch sale_url(sale), params: { sale: new_attributes }
-        sale.reload
-        skip("Add assertions for updated state")
-      end
-
-      it "redirects to the sale" do
-        sale = Sale.create! valid_attributes
-        patch sale_url(sale), params: { sale: new_attributes }
-        sale.reload
-        expect(response).to redirect_to(sale_url(sale))
-      end
-    end
-
-    context "with invalid parameters" do
-      it "renders a successful response (i.e. to display the 'edit' template)" do
-        sale = Sale.create! valid_attributes
-        patch sale_url(sale), params: { sale: invalid_attributes }
-        expect(response).to be_successful
-      end
-    end
-  end
-
-  describe "DELETE /destroy" do
-    it "destroys the requested sale" do
-      sale = Sale.create! valid_attributes
-      expect {
-        delete sale_url(sale)
-      }.to change(Sale, :count).by(-1)
-    end
-
-    it "redirects to the sales list" do
-      sale = Sale.create! valid_attributes
-      delete sale_url(sale)
-      expect(response).to redirect_to(sales_url)
-    end
-  end
 end
